73f5a505c0ec18cea6ce64763ff58d046c8c1de1
Rux Cavescu <ruxcavescu@Ruxs-Laptop.local>
2024-07-29 21:55:28 +0200

    refactor

--- tests/veeam.spec.js
+++ tests/veeam.spec.js
@@ -7,8 +7,6 @@ test('thrown error when using public domains', async ({page}) => {
         email: 'inreviewuser@gmail.com',
     }
 
-    const warningText = "Please note that you will need to enter a valid email address before your account is activated. You will receive an email at the address you provide that contains an account activation link."
-
     // load page and check if it's the correct page
     await page.goto("https://www.veeam.com/");
     await expect(page).toHaveTitle("#1 Global Leader in Data Resilience");
@@ -23,10 +21,10 @@ test('thrown error when using public domains', async ({page}) => {
     await supportButton.click();
 
     // locate and click R&D Forums button
-    const forumsButton = page.locator('a.list-of-links__link', {hasText: 'R&D Forums'});
+    const forumsButton = page.locator('a', {hasText: 'R&D Forums'});
     await expect(forumsButton).toBeVisible();
     await forumsButton.click();
-    
+
     // wait for the page to load and check URL change
     await page.waitForLoadState();
     await expect(page).toHaveURL('https://forums.veeam.com/?ad=menu-support');
@@ -35,31 +33,29 @@ test('thrown error when using public domains', async ({page}) => {
     const registerButton = page.getByRole('menuitem', {name: 'Register'});
     await expect(registerButton).toBeVisible();
     await registerButton.click();
+
+    // check if correct redirect
+    await page.waitForLoadState();
+    await expect(page).toHaveTitle('R&D ForumsUser Control Panel - ');
+    // looks like the page has a temporary id in the url as well
+    await expect(page).toHaveURL(/https:\/\/forums\.veeam\.com\/ucp\.php\?mode=register.*/);
     
+    // locate and click agree to terms button
+    const agreeButton = page.getByText('I agree to these terms')
+
+    // since the element does not have a visibility property, we'll check for display:
+    const agreeButtonStyle = await agreeButton.evaluate((elem) => window.getComputedStyle(elem).display);
+    // check that the display style is not 'none'
+    expect(agreeButtonStyle).not.toBe('none');
+    await agreeButton.click();
+    
+    await expect(page).toHaveURL('https://forums.veeam.com/ucp.php?mode=register');
     await page.waitForLoadState();
 
     // check if correct redirect
-    await expect(page).toHaveTitle('R&D ForumsUser Control Panel - ');
-    
-    // locate and click agree to terms button
-    const agreeButton = page.getByRole('button', {name: 'I agree to these terms'});
-    // await expect(agreeButton).toBeVisible();
-    const agreeButtonStyle = await agreeButton.evaluate((elem) => window.getComputedStyle(elem).display);
-    // Check that the display style is not 'none'
-    expect(agreeButtonStyle).not.toBe('none'); // Ensures the element is not hidden using display: none;
-    await agreeButton.click();
-    
-    // looks like the page has a temporary id in the url as well 
-    await page.waitForLoadState();
-    await expect(page).toHaveURL(/https:\/\/forums\.veeam\.com\/ucp\.php\?mode=register.*/);
-    
-    // check if correct redirect
-    const warningBox = page.locator('dd').filter({ hasText: `${warningText}` })
-    // since the element does not have a visibility property, we'll check for display:
-    const displayWarningBoxStyle = await warningBox.evaluate((elem) => window.getComputedStyle(elem).display);
-    // Check that the display style is not 'none'
-    expect(displayWarningBoxStyle).not.toBe('none'); // Ensures the element is not hidden using display: none;
-    // await expect(warningBox).toBeVisible();
+    const submitButton = page.getByRole('button', { name: 'Submit' });
+    const submitButtonStyle = await submitButton.evaluate((elem) => window.getComputedStyle(elem).display);
+    expect(submitButtonStyle).not.toBe('none');
     
     // fill in registration form
     await page.getByLabel('Username:').fill(`${mockRegisterData.username}`);
@@ -67,15 +63,14 @@ test('thrown error when using public domains', async ({page}) => {
     await page.getByLabel('Confirm password:', {exact: true}).fill(`${mockRegisterData.password}`);
     await page.getByLabel('Email address:').fill(`${mockRegisterData.email}`);
     
-    const submitButton = page.getByRole('button', { name: 'Submit' });
     await submitButton.click();
     
+    // wait for the page to load
     await page.waitForLoadState();
 
+    // locate the error component
     const errorBox = page.locator('dd.error');
     await errorBox.waitFor();
-    // await expect(errorBox).toBeVisible();
     const displayErrorBoxStyle = await errorBox.evaluate((elem) => window.getComputedStyle(elem).display);
-    // Check that the display style is not 'none'
     expect(displayErrorBoxStyle).not.toBe('none');
 })
